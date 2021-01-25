43f719ab059cee5620338a87248191c140933668
madkeakash12 <madkeakash12@users.noreply.github.com>
2021-01-25 09:55:00 +0530

    Modified model class

--- src/main/java/com/app/model/EmployeeModel.java
+++ src/main/java/com/app/model/EmployeeModel.java
@@ -4,5 +4,12 @@ public class EmployeeModel {
 
 	private int empId;
 	private String empName;
+	public EmployeeModel(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	
 
 }
